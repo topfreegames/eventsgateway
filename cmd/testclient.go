b1ae6ad5f0f5e076644c551a22dfe12e25c1c486
Yuri Brito <yuriabrito@gmail.com>
2019-04-23 18:10:27 -0300

    testclient & local-stack: s3 + gobblin + hive

--- cmd/testclient.go
+++ cmd/testclient.go
@@ -20,12 +20,35 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-package main
+package cmd
 
 import (
-	"github.com/topfreegames/eventsgateway/cmd"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/topfreegames/eventsgateway/testclient"
 )
 
-func main() {
-	cmd.Execute()
+// testClient represents the testclient command
+var testClient = &cobra.Command{
+	Use:   "testclient",
+	Short: "runs a test client",
+	Long:  `runs a test client`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log := logrus.New()
+		if debug {
+			log.SetLevel(logrus.DebugLevel)
+		}
+		if json {
+			log.Formatter = new(logrus.JSONFormatter)
+		}
+		tc, err := testclient.NewTestClient(log, config)
+		if err != nil {
+			log.Panic(err)
+		}
+		tc.Run()
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(testClient)
 }
