6e79495033ba7e6d2be25e785c8286c1857601de
Yuri Brito <yuriabrito@gmail.com>
2019-05-16 16:03:37 -0300

    using tfgco/eventsgateway as docker REPO

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -7,21 +7,21 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright © 2018 Top Free Games <backend@tfgco.com>
 
-REPO=eventsgateway
+REPO=tfgco/eventsgateway
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
 echo "sending image to dockerhub"
-docker build -t eventsgateway .
+docker build -t ${REPO} .
 
 # If this is not a pull request, update the branch's docker tag.
 if [ $TRAVIS_PULL_REQUEST = 'false' ]; then
-  docker tag eventsgateway:latest eventsgateway:${TRAVIS_BRANCH/\//-} \
-    && docker push eventsgateway:${TRAVIS_BRANCH/\//-};
+  docker tag ${REPO}:latest ${REPO}:${TRAVIS_BRANCH/\//-} \
+    && docker push ${REPO}:${TRAVIS_BRANCH/\//-};
 
   # If this commit has a tag, use on the registry too.
   if ! test -z $TRAVIS_TAG; then
-    docker tag eventsgateway:latest $REPO:${TRAVIS_TAG} \
+    docker tag ${REPO}:latest $REPO:${TRAVIS_TAG} \
       && docker push $REPO:${TRAVIS_TAG};
   fi
 fi
